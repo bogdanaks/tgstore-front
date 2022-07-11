50fc3479a94d8413c893c8a5fe371dcabcb89e90
bogdanaks <bogdanaks@bk.ru>
2022-07-12 00:37:49 +0300

    Add types

--- src/shared-kernel.d.ts
+++ src/shared-kernel.d.ts
@@ -1,3 +1,26 @@
+type WebAppType = {
+  id: string
+  title: string
+  slug: string
+  created_at: Date
+  updated_at: Date
+}
+
+type WebAppCategory = {
+  id: string
+  title: string
+  slug: string
+  created_at: Date
+  updated_at: Date
+}
+
+enum WebAppStatus {
+  DRAFT = "draft",
+  PUBLISHED = "published",
+  REVIEWING = "reviewing",
+  CANCELED = "canceled",
+}
+
 type WebApp = {
   id: string
   title: string
@@ -13,4 +36,81 @@ type WebApp = {
   category: WebAppCategory
   created_at: Date
   updated_at: Date
+}
+
+interface TgWebApp {
+  expand: () => void
+  openTelegramLink: (url: string) => void
+  openLink: (url: string) => void
+  setHeaderColor: (rgb: string) => void
+  setBackgroundColor: (rgb: string) => void
+  setText: (text: string) => void
+  initData: string
+  initDataUnsafe: {
+    query_id: string
+    user: {
+      id: number
+      first_name: string
+      last_name: string
+      language_code: string
+    }
+    auth_date: string
+    hash: string
+  }
+  version: string
+  colorScheme: string
+  themeParams: {
+    bg_color: string
+    secondary_bg_color: string
+    button_color: string
+    button_text_color: string
+    hint_color: string
+    link_color: string
+    text_color: string
+  }
+  isExpanded: boolean
+  viewportHeight: number
+  viewportStableHeight: number
+  headerColor:string
+  backgroundColor: string
+  BackButton: {
+    isVisible: boolean
+    onClick: (cb: unknown) => void
+    offClick: (cb: unknown) => void
+    show: () => void
+    hide: () => void
+  }
+  MainButton: {
+    text: string
+    color: string
+    textColor: string
+    isVisible: boolean
+    isProgressVisible: boolean
+    isActive: boolean
+    setText: (text: string) => void
+    show: () => void
+    hide: () => void
+    onClick: (cb: unknown) => void
+    offClick: (cb: unknown) => void
+  },
+  HapticFeedback: unknown
+}
+
+interface TgWebView {
+  callEventCallbacks: (eventType: string, func: any) => void
+  initParams: object
+  isIframe: false
+  offEvent: (eventType: string, func: any) => void
+  onEvent: (eventType: string, func: any) => void
+  postEvent: (eventType: string, func: () => void, eventData: any) => void
+  receiveEvent: (eventType: string, func: () => void, eventData: any) => void
+}
+
+export declare global {
+  interface Window {
+    Telegram: {
+      WebApp: TgWebApp
+      WebView: TgWebView
+    }
+  }
 }
