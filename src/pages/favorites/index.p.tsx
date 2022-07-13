bbff4619265bb1138fb37368dbd56839be49a1b9
bogdanaks <bogdanaks@bk.ru>
2022-07-14 00:49:15 +0300

    Create pages

---
+++ src/pages/favorites/index.p.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Wrapper } from "shared/ui/wrapper"
+import { Footer } from "widgets/footer"
+
+const FavoritesPage = () => {
+  return (
+    <Wrapper>
+      <div>Fav</div>
+      <Footer />
+    </Wrapper>
+  )
+}
+
+export default FavoritesPage
---
+++ src/pages/search/index.p.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Wrapper } from "shared/ui/wrapper"
+import { Footer } from "widgets/footer"
+
+const SearchPage = () => {
+  return (
+    <Wrapper>
+      <div>SearchPage</div>
+      <Footer />
+    </Wrapper>
+  )
+}
+
+export default SearchPage
