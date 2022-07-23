c7a5eef40b4d920941f58f95c32d3eeeca79922e
bogdanaks <bogdanaks@bk.ru>
2022-07-23 19:36:46 +0300

    Add provider

--- src/pages/_app.p.tsx
+++ src/pages/_app.p.tsx
@@ -1,13 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import type { AppProps } from "next/app"
 import LocationProvider from "processes/location"
+
+const queryClient = new QueryClient()
 
 import "/styles/globals.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <LocationProvider>
-      <Component {...pageProps} />
-    </LocationProvider>
+    <QueryClientProvider client={queryClient}>
+      <LocationProvider>
+        <Component {...pageProps} />
+      </LocationProvider>
+    </QueryClientProvider>
   )
 }
 
