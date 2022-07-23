dff7048769545bd85dfb765da296986c03638f23
bogdanaks <bogdanaks@bk.ru>
2022-07-23 19:38:01 +0300

    Update

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -18,7 +18,27 @@ const fetcher = async <T>(input: RequestInfo, init?: RequestInit) => {
 
 export const getAppById = async (appId: string): Promise<WebApp> => {
   try {
-    return await fetcher<WebApp>(`${config.API_URL}/app/${appId}`, { // TODO add type
+    return await fetcher<WebApp>(`${config.API_URL}/app/${appId}`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  } catch (error) {
+    if (error instanceof Response) {
+      console.log(await error.text())
+      
+      const msg: ResponseError = JSON.parse(await error.text())
+      const msgFormatting = Array.isArray(msg.message) ? msg.message.join(", ") : msg.message
+      throw new Error(msgFormatting)
+    }
+    throw new Error(`Something went wrong: ${error}`)
+  }
+}
+
+export const getCategories = async (): Promise<CategoryApp[]> => {
+  try {
+    return await fetcher<CategoryApp[]>(`${config.API_URL}/app-category`, {
       method: "GET",
       headers: {
         Accept: "application/json",
