80bd6e285533a8d72a5c7ace8486b70887e55eb9
bogdanaks <bogdanaks@bk.ru>
2022-07-23 19:39:04 +0300

    Update wrapper

--- src/shared/ui/wrapper/index.tsx
+++ src/shared/ui/wrapper/index.tsx
@@ -1,7 +1,15 @@
-import React, { FC } from "react"
+import React, { FC, HTMLAttributes } from "react"
 
 import styles from "./styles.module.scss"
 
-export const Wrapper: FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className={styles.wrapper}>{children}</div>
+interface WrapperProps extends HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode
+}
+
+export const Wrapper: FC<WrapperProps> = ({ children, ...props }) => {
+  return (
+    <div className={styles.wrapper} style={{ ...props.style }}>
+      {children}
+    </div>
+  )
 }
