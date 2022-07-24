ced9f557f514b21c5c230ce5041cf04d9459453a
bogdanaks <bogdanaks@bk.ru>
2022-07-24 15:52:06 +0300

    Improve

--- src/widgets/search/ui.tsx
+++ src/widgets/search/ui.tsx
@@ -1,16 +1,31 @@
-import React from "react"
+import React, { ChangeEvent, FC, HTMLAttributes, MutableRefObject } from "react"
 import { IoSearchOutline } from "react-icons/io5"
 import { Input } from "shared/ui/input"
 
 import styles from "./styles.module.scss"
 
-export const Search = () => {
+interface SearchProps extends HTMLAttributes<HTMLInputElement> {
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  autoFocus?: boolean
+}
+
+export const Search: FC<SearchProps> = ({
+  value,
+  onChange,
+  autoFocus,
+  ...props
+}) => {
   return (
     <div className={styles.wrapper}>
       <Input
         style={{ height: 35 }}
         placeholder="Search..."
         leftContent={<IoSearchOutline />}
+        value={value}
+        onChange={onChange}
+        autoFocus={autoFocus}
+        {...props}
       />
     </div>
   )
