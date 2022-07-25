184eed28a4c7222b02d67234092b53d71dca584a
bogdanaks <bogdanaks@bk.ru>
2022-07-25 23:06:17 +0300

    Improve

--- src/pages/_document.p.tsx
+++ src/pages/_document.p.tsx
@@ -19,6 +19,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
